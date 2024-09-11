e3479816797df05eb0902bc6f0d186d68ae2b49b
gdelcroix <4097422+gdelcroix@users.noreply.github.com>
2024-09-11 13:19:24 +0200

    point virgule perdu lors du pull sur github + alignement logo correction orthographe dans le dictionnaire insertion de la génération de mot aléatoire

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const listeMots = [AALENIEN,ABAISSEE];
-    const mot = listeMots[Math.floor(Math.random()*listeMots.length)];
+// document.addEventListener('DOMContentLoaded', () => {
+    // ajoute le dictionnaire stocké dans un fichier distinct
+    import { dictionnaire_list } from "./dictionnaire.js"; 
+        
+    // choisit le nombre de lettre nécessaire
+    const nbLettres = Math.floor(Math.random() * 4);
+    // choisit le mot dans le dictionnnaire selon le nombre de lettres
+    function choixmotparlongueur(longueur) {
+        const liste = dictionnaire_list[longueur];
+        const indexAleatoire = Math.floor(Math.random() * liste.length);
+        return liste[indexAleatoire];
+    }
+    
+    const mot = choixmotparlongueur(nbLettres);
+    
     console.log(mot);
 
-    document.createElement('div')
-    
+       
 
-});
+// });
